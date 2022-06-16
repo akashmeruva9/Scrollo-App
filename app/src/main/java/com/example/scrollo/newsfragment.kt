652eeb7917470c78bc7647985154744a865291e8
Akash Meruva <aksh.meruva2003@gmail.com>
2022-06-16 12:58:46 +0530

    Scrollo App

--- app/src/main/java/com/example/scrollo/newsfragment.kt
+++ app/src/main/java/com/example/scrollo/newsfragment.kt
@@ -1,7 +1,6 @@
 package com.example.scrollo
 
 import android.annotation.SuppressLint
-import android.content.Context
 import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
@@ -9,7 +8,6 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
 import android.widget.Toast
 import androidx.browser.customtabs.CustomTabsIntent
 import androidx.recyclerview.widget.LinearLayoutManager
